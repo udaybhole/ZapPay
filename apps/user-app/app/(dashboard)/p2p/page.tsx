d4318b6ea5ad6d3dbf861104a5d7ecc4bc52c80c
udaybhole <udaybhole3104@gmail.com>
2024-11-17 21:00:17 +0530

    Done with P2P transaction Merchant and dashboard remaining

---
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -0,0 +1,35 @@
+import prisma from "@repo/db/client";
+import { SendCard } from "../../../components/SendCard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../lib/auth";
+import { P2pTransactions } from "../../../components/P2pTransaction";
+
+const getp2ptransaction = async () => {
+  const session = await getServerSession(authOptions);
+  const transactions = await prisma.p2pTransfer.findMany({
+    where: {
+      OR: [
+        { fromUserId: Number(session?.user?.id) },
+        { toUserId: Number(session?.user?.id) },
+      ],
+    },
+    orderBy: {
+      timestamp: "desc",
+    },
+  });
+  return transactions;
+};
+
+export default async function () {
+  const transactions = await getp2ptransaction();
+  return (
+    <div className="flex justify-center items-center w-[80vw] gap-20">
+      <div className="w-[30%]">
+        <SendCard />
+      </div>
+      <div className="w-[60%]">
+        <P2pTransactions transactions={transactions} />
+      </div>
+    </div>
+  );
+}
