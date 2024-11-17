c03e63bb33bf3022fed50408886898f123d22741
udaybhole <udaybhole3104@gmail.com>
2024-11-17 16:10:29 +0530

    All At once op to save time

--- apps/user-app/components/AddMoneyCard.tsx
+++ apps/user-app/components/AddMoneyCard.tsx
@@ -5,7 +5,8 @@ import { Center } from "@repo/ui/center";
 import { Select } from "@repo/ui/select";
 import { useState } from "react";
 import { TextInput } from "@repo/ui/textinput";
-
+import { useRouter } from "next/navigation";
+import db from "@repo/db/client"
 const SUPPORTED_BANKS = [{
     name: "HDFC Bank",
     redirectUrl: "https://netbanking.hdfcbank.com"
@@ -16,6 +17,7 @@ const SUPPORTED_BANKS = [{
 
 export const AddMoney = () => {
     const [redirectUrl, setRedirectUrl] = useState(SUPPORTED_BANKS[0]?.redirectUrl);
+    const router = useRouter();
     return <Card title="Add Money">
     <div className="w-full">
         <TextInput label={"Amount"} placeholder={"Amount"} onChange={() => {
@@ -32,7 +34,14 @@ export const AddMoney = () => {
         }))} />
         <div className="flex justify-center pt-4">
             <Button onClick={() => {
+
+                
+
+                
+
+
                 window.location.href = redirectUrl || "";
+
             }}>
             Add Money
             </Button>
