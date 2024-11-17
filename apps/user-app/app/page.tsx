eb99c1ba38e4a50867696f7fb237586ba4b86a83
udaybhole <udaybhole3104@gmail.com>
2024-11-17 22:42:10 +0530

    Done with transaction in user-app

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth";
 import { redirect } from 'next/navigation'
 import { authOptions } from "./lib/auth";
-
+import { balance } from "./lib/actions/balance";
 export default async function Page() {
   const session = await getServerSession(authOptions);
   if (session?.user) {
@@ -9,4 +9,7 @@ export default async function Page() {
   } else {
     redirect('/api/auth/signin')
   }
+
+
+  
 }
