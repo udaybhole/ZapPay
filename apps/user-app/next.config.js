29b3b2fa899a6d3559dae5dda930823390c3b3ec
udaybhole <udaybhole3104@gmail.com>
2024-11-21 18:07:49 +0530

    deploying last time

--- apps/user-app/next.config.js
+++ apps/user-app/next.config.js
@@ -2,3 +2,15 @@
 module.exports = {
   transpilePackages: ["@repo/ui"],
 };
+
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
+}
